810d9969b86d1c5bb87752d9704f9bfd981036f7
Ayaz Duru <ayzdru@outlook.com>
2020-07-24 02:40:57 +0300

    Proje ad ve dosya yolları düzenlendi.

---
+++ source/AspNetInMemoryAndDistributedCaching/Pages/DistributedCache.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetInMemoryAndDistributedCaching.Pages
+{
+    public class DistributedCacheModel : PageModel
+    {
+        private readonly ILogger<DistributedCacheModel> _logger;
+        private readonly IDistributedCache _distributedCache;
+        public DistributedCacheModel(ILogger<DistributedCacheModel> logger, IDistributedCache distributedCache)
+        {
+            _logger = logger;
+            _distributedCache = distributedCache;
+        }
+
+        public WeatherForecast WeatherForecast { get; set; }
+        public async Task OnGetAsync()
+        {
+            var weatherForecastJsonString = await _distributedCache.GetStringAsync("WeatherForecast");
+
+            if (weatherForecastJsonString != null)
+            {
+                WeatherForecast = JsonSerializer.Deserialize<WeatherForecast>(weatherForecastJsonString);
+            }
+        }
+    }
+}
