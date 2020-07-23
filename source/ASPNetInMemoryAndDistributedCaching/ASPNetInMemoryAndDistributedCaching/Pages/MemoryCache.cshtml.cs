b41d62ee33e5ac44f6e517ea69b8aef962364851
Ayaz Duru <ayzdru@outlook.com>
2020-07-23 15:05:54 +0300

    Proje oluşturuldu.

---
+++ source/ASPNetInMemoryAndDistributedCaching/ASPNetInMemoryAndDistributedCaching/Pages/MemoryCache.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNetInMemoryAndDistributedCaching.Pages
+{
+    public class MemoryCacheModel : PageModel
+    {
+        private readonly ILogger<MemoryCacheModel> _logger;
+        private readonly IMemoryCache _memoryCache;
+        public MemoryCacheModel(ILogger<MemoryCacheModel> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+        public WeatherForecast WeatherForecast { get; set; }
+        public void OnGet()
+        {
+            _memoryCache.TryGetValue<WeatherForecast>("WeatherForecast", out WeatherForecast weatherForecast);
+            if (weatherForecast !=null)
+            {
+                WeatherForecast = weatherForecast;
+            }
+        }
+    }
+}
---
+++ source/ASPNetInMemoryAndDistributedCaching/ASPNetInMemoryAndDistributedCaching/Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ASPNetInMemoryAndDistributedCaching
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddDistributedMemoryCache();
+            services.AddMemoryCache();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime, IDistributedCache cache, IMemoryCache memoryCache)
+        {
+            lifetime.ApplicationStarted.Register(() =>
+            {
+                var options = new DistributedCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(20));
+                var weatherForecast = new WeatherForecast
+                {
+                    Date = DateTime.Now,
+                    Summary = "Freezing",
+                    TemperatureCelsius = -20
+                };
+                memoryCache.Set("WeatherForecast", weatherForecast);
+                var jsonString = JsonSerializer.Serialize(weatherForecast);
+                cache.SetString("WeatherForecast", jsonString, options);
+            });
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
